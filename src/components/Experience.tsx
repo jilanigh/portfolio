c303fe8ce94ea16fa7aed9c178cdd8346efae11a
gharbijilani <gharbijilani001@gmail.com>
2026-04-25 18:53:32 +0100

    add new experience

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -6,13 +6,6 @@ import { useState } from "react";
 
 const experiences = [
   {
-    title: "Can You Run It",
-    company: "Personal Project",
-    date: "2024 - Present",
-    description: "Developed a hardware-aware gaming web app that detects PC specs and checks game compatibility. Built a real-world system combining React + Vite frontend, Express backend, and IGDB API integration. Implemented custom compatibility scoring logic and AI guidance to help users estimate performance before installation. (https://can-you-run-it.vercel.app)",
-    tech: "React, Vite, Express, IGDB API, Node.js, Framer Motion, i18n"
-  },
-  {
     title: "AWS Trainer",
     company: "TEK-UP",
     date: "03/2025 - Present",
